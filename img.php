bc9eb682b1eca205407f204cfe8a7b57bf111cf8
Aniket Kavathekar <57369958+nick-hack@users.noreply.github.com>
2021-08-24 08:27:31 +0530

    student one
    changes first

---
+++ img.php
@@ -0,0 +1,9 @@
+<?php
+
+    <div class="form-group">
+        		<label>Image</label>
+        		<input type="file" name="image" class="form-control" >
+        </div>
+            
+    <input type="submit" name="submit" class="btn btn-info btn-large" value="Submit">
+?>
